32a670b3dc1479972f51097eea35b8a561ce19fc
Sven Larson <svenlarson1098@gmail.com>
2018-04-08 19:42:55 +0000

    Lesson 7 Assignment Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'random_data'
+
+#create posts
+50.times do
+    
+  Post.create!(
+         
+    title: RandomData.random_sentence, 
+    body: RandomData.random_paragraph
+  )
+      
+end
+posts = Post.all
+
+#Create Comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+    )
+end
+
+puts "#{Post.count}"
+Post.find_or_create_by(title: "Try and find me!", body: "Find my elusive unique post amongst these garbled jargon entries!")
+puts "#{Post.count}"
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
