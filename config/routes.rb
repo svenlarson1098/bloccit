5d1ab2028d78984e2996f4877f77b9dbca4d9144
Sven Larson <svenlarson1098@gmail.com>
2018-04-18 20:43:41 +0000

    Lesson 9 assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   
+  #get 'advertisements/index'
+
+  #get 'advertisements/show'
+
+  #get 'advertisements/new'
+
+  #get 'advertisements/create'
+
+  resources :advertisements
   resources :posts
 
   #get 'welcome/contact'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406151522) do
+ActiveRecord::Schema.define(version: 20180417163719) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
