a6d2e0ff9fde12b9f84bb1677f7e2c7cbc5d5d41
Sven Larson <svenlarson1098@gmail.com>
2018-04-27 17:30:33 +0000

    Lesson 11 Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :topics
-  resources :posts
+  resources :topics do
+  resources :posts, except: [:index]
+end
+
   resources :questions
   resources :advertisements
   #get 'welcome/contact'
