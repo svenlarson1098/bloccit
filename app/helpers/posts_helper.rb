ec2ef38640283ca6cf86d0b737a6130a9c235760
Sven Larson <svenlarson1098@gmail.com>
2018-05-17 18:27:08 +0000

    Revision Request for Authentication Moderator Assignment 18

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,9 @@
 module PostsHelper
   def user_is_authorized_for_post?(post)
-    current_user && (current_user == post.user || current_user.admin? || current_user.moderator?)
+    current_user && (current_user == post.user || current_user.admin?)
+  end
+  
+  def mod_user
+    current_user.moderator?
   end
 end
--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,9 @@
 module TopicsHelper
   def user_is_authorized_for_topics?
-    current_user && (current_user.admin? || current_user.moderator?)
+    current_user && current_user.admin?
+  end
+  
+  def mod_user
+    current_user && current_user.moderator?
   end
 end
